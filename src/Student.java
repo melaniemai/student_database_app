b6848e10f6ac1d767da8af226bb7d892ef219400
Melanie Mai <melaniesmai@outlook.com>
2020-11-11 00:22:49 -0500

    Create outline for Student class

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuition;
+    private int courseCost = 600; // default price
+
+    // Constructor:
+
+    // Create student ID: 5-digit unique ID
+
+    // Enroll in courses
+
+    // View student's balance
+
+    // Pay student tuition
+
+    // Display student stats/info: name, ID, enrolled courses, and balance
+}
---
+++ src/StudentDBApp.java
@@ -0,0 +1,7 @@
+public class StudentDBApp {
+    public static void main(String[] args) throws Exception {
+        // Query number of students to add
+
+        // Create x amount of students
+    }
+}
