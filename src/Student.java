6a3a095a88e37d5cd7903a344d407cc22d484043
Melanie Mai <melaniesmai@outlook.com>
2020-11-11 17:02:20 -0500

    Updated Student constructor

--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int tuition;
-    private int courseCost = 600; // default price
+    private int courseCost = 500; // default price
 
-    // Constructor:
+    // Constructor
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("-- STUDENT STATUS --");
+        System.out.println("1. Freshman");
+        System.out.println("2. Sophomore");
+        System.out.println("3. Junior");
+        System.out.println("4. Senior");
+        System.out.print("Enter student status (1-4): ");
+        this.gradeYear = in.nextInt();
+
+        System.out.print("Welcome " + firstName + " " + lastName + ": " + gradeYear);
+    }
 
     // Create student ID: 5-digit unique ID
 
--- src/StudentDBApp.java
+++ src/StudentDBApp.java
@@ -1,5 +1,7 @@
 public class StudentDBApp {
     public static void main(String[] args) throws Exception {
+        Student s1 = new Student();
+
         // Query number of students to add
 
         // Create x amount of students
