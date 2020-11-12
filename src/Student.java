0ba540be1fde6dc34bd714d9fbe2cc78d912c84c
Melanie Mai <melaniesmai@outlook.com>
2020-11-11 21:05:02 -0500

    Created student ID,

--- src/Student.java
+++ src/Student.java
@@ -5,10 +5,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tuition;
-    private int courseCost = 500; // default price
+    private static int courseCost = 500; // default price
+    private static int id = 10000;
 
     // Constructor
     public Student() {
@@ -27,12 +28,26 @@ public class Student {
         System.out.print("Enter student status (1-4): ");
         this.gradeYear = in.nextInt();
 
-        System.out.print("Welcome " + firstName + " " + lastName + ": " + gradeYear);
+        setStudentID();
+        System.out.println("Welcome " + firstName + " " + lastName + ": " + gradeYear);
+        System.out.println("Student ID: " + studentID);
+
     }
 
     // Create student ID: 5-digit unique ID
+    // * Student ID: Grade level + ID
+    private void setStudentID() {
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
 
     // Enroll in courses
+    public void enroll() {
+        // * User will continue to enroll in courses through a loop
+        // * Once the user enters the number '0', the loop will terminate
+        // * indicating the user is done enrolling in their courses
+        System.out.print("Enter course to enroll (0 to quit): ");
+    }
 
     // View student's balance
 
