8a668ea3bea65eaa50b2470fb5c58e720ad20dbc
Melanie Mai <melaniesmai@outlook.com>
2020-11-11 23:59:44 -0500

    Created multiple students

--- src/StudentDBApp.java
+++ src/StudentDBApp.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class StudentDBApp {
     public static void main(String[] args) throws Exception {
-        Student s1 = new Student();
-        s1.enroll();
-
         // Query number of students to add
+        System.out.print("Number of students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         // Create x amount of students
+        for (int i = 0; i < numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+        }
+
+        for (int i = 0; i < numOfStudents; i++) {
+            System.out.println(students[i].showInfo());
+        }
     }
 }
